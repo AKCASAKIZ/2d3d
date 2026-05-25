df674eac0b98c562f8f6285e99921e9c2d05568b
whitegum <114851839+AKCASAKIZ@users.noreply.github.com>
2026-05-25 14:07:17 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -50,6 +50,7 @@ export interface PathSettings {
   depth: number;
   revolveAxis?: 'left' | 'center' | 'right' | 'origin-y' | 'origin-x';
   booleanType?: 'union' | 'cut';
+  groupId?: string;
 }
 
 export interface CADLayer {
